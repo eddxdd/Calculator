e7b1b375a4bff3c89fcc2a4019ba711de2c52ecf
Eduardo Lemos <31359604+eddxdd@users.noreply.github.com>
2017-10-22 18:10:46 -0400

    Update calculator.js

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,8 +1,6 @@
-
-
 $(document).ready(function () {
   // Variables
-  var input = [""];
+  var inputVal = [""];
   var totalValue;
 
   // Operators validation
@@ -13,30 +11,58 @@ $(document).ready(function () {
   // Main function updates all
   function getValue(value) {
 
+    // Disable duplicate '.' operator
+    if(operator2.includes(inputVal[inputVal.length-1] === true && value === ".")) {
+      console.log("Duplicated '.' ");
+    }
 
+    // Cannot start with an operator
+    else if(inputVal.length === 1 && operator1.includes(value) === false) {
+      inputVal.push(value);
+    }
+
+    // Check if last input was an operator ++ -- **
+    else if(operator1.includes(inputVal[inputVal.length-1] === false)) {
+      inputVal.push(value);
+    }
+    else if(nums.includes(Number(value))) {
+      inputVal.push(value);
+    }
+    update(); // Updates function after checking validity of one of the above
   }
+
   // This function will update the display everytime we input a new key
   function update() {
-    totalValue = input.join("");
+    totalValue = inputVal.join("");
     $("#steps").html(totalString);
-
+    console.log(inputVal);
   }
+
   function getTotal() {
-    totalValue = input.join("");
+    totalValue = inputVal.join("");
     $("#steps").html(eval(totalValue)); // Simply evaluates the string
   }
+
   $("a").on("click", function() { // Make buttons do stuff
 
     if(this.id==="delAll") {
-      input=[""];
+      inputVal = [""];
       update(); // Delete all inputs and update to most recent
     }
     else if(this.id==="delOne") {
-      input.pop();
+      inputVal.pop();
       update();
     }
     else if(this.id==="total") {
       getTotal();
+    }
+    else {
+      if(inputVal[inputVal.length-1].indexOf("%", "/", "*", "+", "-", "-1") === -1){
+        getValue(this.id);
+      }
+      else {
+        getValue(this.id);
+      }
     }
   });
 
