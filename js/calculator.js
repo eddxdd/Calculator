27edc4b186bcfec7d1a2b31cd452c5efa71daaa5
Eduardo Lemos <eddu1930@gmail.com>
2017-12-03 17:55:07 -0500

    Add files via upload

--- js/calculator.js
+++ js/calculator.js
@@ -1,69 +1,95 @@
-$(document).ready(function () {
-  // Variables
-  var inputVal = [""];
-  var totalValue;
+var result = document.querySelector(".result");
+    var btns = document.querySelector(".pad");
+    var ops = document.querySelectorAll(".operator");
+    var clearToggle = false;
+    var timeFlag = false;
+    var valStack = [];
+    var canOperate = false;
+    btns.addEventListener("click",function(e){
+        var resultValue = result.innerHTML;
+        var btn = e.target;
 
-  // Operators 
-  var operator1 = ["%", "/", "*", "+", "-"];
-  var operator2 = ["."];
-  var nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        var selected = document.querySelector(".selected");
+        if(null !== selected) selected.classList.remove("selected");
 
-  // Main function updates all
-  function getValue(value) {
+        if(btn.className.indexOf("resetBtn") > -1){
+           init();
+        }else if(btn.className.indexOf("number") > -1){
+            canOperate = true;
+            if(clearToggle){
+                resultValue = "";
+                clearToggle = false;
+            }
+            if(resultValue == '0' && btn.className.indexOf("dot") === -1){
+                resultValue = "";
+            }
+            result.innerHTML = resultValue + btn.innerHTML;
+        }else {
+            clearToggle = true;
+            if(canOperate){
+                btn.classList.add("selected");
 
-    // Disable duplicate '.' operator
-    if(operator2.includes(inputVal[inputVal.length-1] === true && value === ".")) {
-      console.log("Duplicated '.' ");
+                if(btn.innerHTML === '*' || btn.innerHTML === '/'){
+                    if(!timeFlag){
+                        valStack.push(resultValue);
+                        valStack.push(btn.innerHTML);
+                        timeFlag = true;
+                        return;
+                    }
+                }else if(btn.innerHTML === '%'){
+                    resultValue = parseFloat(resultValue/100);
+                   // valStack.push(resultValue);
+                    result.innerHTML = resultValue;
+                    return;
+                }
+
+                if(valStack.length > 1){
+                    var op = valStack.pop();
+                    var num1 = valStack.pop();
+                    resultValue = calculate(num1,resultValue,op);
+                    if((btn.innerHTML === '+' || btn.innerHTML === '-'|| btn.innerHTML === '=') && timeFlag){
+                        while(valStack.length > 1){
+                            op = valStack.pop();
+                            num1 = valStack.pop();
+                            resultValue = calculate(num1,resultValue,op);
+                        }
+                        timeFlag = false;
+                    }
+
+                }
+                valStack.push(resultValue);
+                valStack.push(btn.innerHTML);
+                result.innerHTML = resultValue;
+                if(btn.innerHTML === '='){
+                    clearToggle = true;
+                    timeFlag = false;
+                    valStack = [];
+                    return;
+                }
+            }
+            canOperate = false;
+        }
+
+    },false);
+
+    function calculate(num1,num2,op){
+        switch (op) {
+            case "+":
+                return parseFloat(num1) + parseFloat(num2);
+            case "-":
+                return parseFloat(num1) - parseFloat(num2);
+            case "*":
+                return parseFloat(num1) * parseFloat(num2);
+            case "/":
+                return parseFloat(num1) / parseFloat(num2);
+
+        }
     }
 
-    // Cannot start with an operator
-    else if(inputVal.length === 1 && operator1.includes(value) === false) {
-      inputVal.push(value);
+    function init(){
+        clearToggle = false;
+        timeFlag = false;
+        valStack = [];
+        canOperate = false;
+        result.innerHTML = "0";
     }
-
-    // Check if last input was an operator ++ -- **
-    else if(operator1.includes(inputVal[inputVal.length-1]) === false) {
-      inputVal.push(value);
-    }
-    else if(nums.includes(Number(value))) {
-      inputVal.push(value);
-    }
-    update(); // Updates function after checking validity of one of the above
-  }
-
-  // This function will update the display everytime we input a new key
-  function update() {
-    totalValue = inputVal.join("");
-    $("#steps").html(totalValue);
-    console.log(inputVal);
-  }
-
-  function getTotal() {
-    totalValue = inputVal.join("");
-    $("#steps").html(eval(totalValue)); // Simply evaluates the string
-  }
-
-  $("a").on("click", function() { // Make buttons do stuff
-
-    if(this.id === "delAll") {
-      inputVal = [""];
-      update(); // Delete all inputs and update to most recent
-    }
-    else if(this.id === "delOne") {
-      inputVal.pop();
-      update();
-    }
-    else if(this.id === "total") {
-      getTotal();
-    }
-    else {
-      if(inputVal[inputVal.length-1].indexOf("%", "/", "*", "+", "-", ".") === -1){
-        getValue(this.id);
-      }
-      else {
-        getValue(this.id);
-      }
-    }
-  });
-
-});
